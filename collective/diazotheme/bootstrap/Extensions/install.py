3b4fab45ee14d30306d73b3cb51355ffd01fccf3
adam139 <yuejun.tang@gmail.com>
2019-07-27 11:14:16 +0800

    apply autopep8 and isort

--- collective/diazotheme/bootstrap/Extensions/install.py
+++ collective/diazotheme/bootstrap/Extensions/install.py
@@ -1,18 +1,19 @@
 # -*- coding: utf8 -*-
 
-from collective.diazotheme.bootstrap import logger 
+from collective.diazotheme.bootstrap import logger
 
-#def install(portal, reinstall=False):
+
+# def install(portal, reinstall=False):
 #    # Not needed if you don't need to run different profile conditionally
 #    setup_tool = portal.portal_setup
 #    setup_tool.runAllImportStepsFromProfile('profile-example.gs:default')
 #    logger.info("Installed")
 
 
-
 def uninstall(portal, reinstall=False):
     if not reinstall:
         setup_tool = portal.portal_setup
-        setup_tool.runAllImportStepsFromProfile('profile-collective.diazotheme.bootstrap:uninstall')
+        setup_tool.runAllImportStepsFromProfile(
+            'profile-collective.diazotheme.bootstrap:uninstall')
 
         logger.info("Uninstall done")
--- collective/diazotheme/bootstrap/__init__.py
+++ collective/diazotheme/bootstrap/__init__.py
@@ -1,4 +1,8 @@
 import logging
+
+
 logger = logging.getLogger('collective.diazotheme.bootstrap')
+
+
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
